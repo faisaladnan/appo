9d266574d4281fd30b186bbe320ea94d3885054d
Faisal Adnan <faisal.adnan@gmail.com>
2016-12-31 17:58:33 +0100

    initial commit

---
+++ src/main/java/com/rikit/AppoApplication.java
@@ -0,0 +1,12 @@
+package com.rikit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rikit/AppoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rikit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
